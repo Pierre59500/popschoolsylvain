1f3d9a0abdf310f7fd06c24f2a354dc1e219401f
Pierre59500 <pierrealexrobert21@gmail.com>
2020-10-04 19:14:24 +0200

    js

---
+++ js/script.js
@@ -0,0 +1,265 @@
+//header
+var head = document.createElement("HEAD");
+
+//TITRE
+var titre = document.createElement("TITLE");
+var titretext = document.createTextNode("HTML5 Boilerplate: The web’s most popular front-end template");
+titre.appendChild(titretext);
+document.head.appendChild(titre);
+
+//link
+var link = document.createElement("LINK");
+link.setAttribute("rel", "stylesheet");
+link.setAttribute("type", "text/css");
+link.setAttribute("href", "styles.css");
+document.head.appendChild(x);
+
+//META
+var metaName = document.createElement("META");
+metaName.setAttribute("name", "description");
+metaName.setAttribute("content", "Free Web tutorials");
+
+//container
+let containerDiv = document.createElement("div");
+containerDiv.className = "site-header clearfix site-promo";
+container.setAttributes("role banner");
+
+//DOUTE
+let site-headerclearfixDiv = document.createElement("div");
+site-headerclearfixDiv.className = "site-logo site-nav inline-block-list";
+site-headerclearfixv.setAttributes("h1");
+
+//Logo
+let site-logoDiv = document.createElement("div");
+site-logoDiv.className = "star";
+
+//Star
+let starSpan = document.createElement("span");
+star.setAttributes("*");
+
+//ul li
+let x site-nav inline-block-listUl  = document.createElement("UL");
+let site-nav inline-block-listUl = document.createElement("LI");
+let site-nav inline-block-list = document.createTextNode("...");
+let site-nav inline-block-list = document.createTextNode("...");
+let site-nav inline-block-list = document.createTextNode("...");
+
+
+///------------------------------------------------------
+
+//site-promo
+let site-promoDiv = document.createElement("div");
+site-promoDiv.className = "description cta-option";
+site-promo.setAttributes("h1");
+
+//H1
+let titre = document.createElement("h1");
+let titreH1 = document.createTextNode("The web’s most popular front-end template.");
+titre.appendChild(titreH1);
+document.body.appendChild(titre);
+
+//description
+let descriptionDiv = document.createElement("div");
+descriptionDiv.className = "";
+description.setAttributes("h1");
+document.creatElement("strong");
+
+//paragraphe
+let p = document.createElement("P");
+let paragraphe = document.createTextNode("HTML5 Boilerplate helps you build and web apps or sites. Kick-start your project with the combined knowledge and effort of 100s of developers, all in one little package.");
+p.appendChild(paragraphe);
+document.body.appendChild(p);
+
+//cta-option
+let cta-optionDiv = document.createElement("div");
+cta-optionDiv.className = "";
+cta-option.setAttributes("a");
+
+//btn btn-download
+let btn btn-download = document.createElement("A");
+a.setAttribute("href", "https://github.com/h5bp/html5-boilerplate/releases/download/v8.0.0/html5-boilerplate_v8.0.0.zip");
+
+//last-update
+let last-update = document.createElement("A");
+a.setAttribute("href", "https://github.com/h5bp/html5-boilerplate/releases/download/v8.0.0/html5-boilerplate_v8.0.0.zip");
+
+///------------------------------------------------------
+
+//site section
+let site-sectionDiv = document.createElement("div");
+site-sectionDiv.className = "container";
+site-section.setAttributes("h2");
+
+//container
+let containerDiv = document.createElement("div");
+containerDiv.className = "grid";
+
+//titreH2
+let titre = document.createElement("h2");
+let titreh2 = document.createTextNode("Save time. Create with confidence.");
+titre.appendChild(titreH2);
+document.body.appendChild(titre);
+
+//grid
+let gridDiv = document.createElement("div");
+gridDiv.className = "grid-cell";
+
+//grid-cell
+let grid-cellDiv = document.createElement("div");
+grid-cell.setAttributes("h3, p");
+
+//h3
+let titre = document.createElement("h3");
+let titreH3 = document.createTextNode("...");
+titre.appendChild(titreH3);
+document.body.appendChild(titre);
+
+//p
+let p = document.createElement("P");
+let paragraphe = document.createTextNode("...");
+p.appendChild(paragraphe);
+document.body.appendChild(p);
+
+///------------------------------------------------------
+
+//site-section section-video
+let site-section section-videoDiv = document.createElement('div');
+site-section section-videoDiv.className = 'content';
+let titre = document.createElement("h2");
+let titreh2 = document.createTextNode("Introduction to v8");
+titre.appendChild(titreH2);
+document.body.appendChild(titre);
+
+//content
+let contentDiv = document.createElement("div");
+contentDiv.className = "new";
+content.setAttributes("p, ul");
+let newP = document.createElement("P");
+let paragraphe = document.createTextNode("What's new?");
+newP.appendChild(paragraphe);
+document.body.appendChild(newP);
+
+//ul li
+let new  = document.createElement("UL");
+let new = document.createElement("LI");
+let new = document.createTextNode("...");
+let new = document.createTextNode("Added sample Open Graph metadata");
+let new = document.createTextNode("Updated Modernizr and main.css");
+let new = document.createTextNode("Removed jQuery");
+let new = document.createTextNode("Set anonymizeIp to true in Google Analytics snippet");
+let new = document.createTextNode("Removed Browser Upgrade Prompt");
+
+//site=section
+let site-sectionDiv = document.createElement("div");
+site-sectionDiv.className = "cta-option";
+site-section.setAttributes("h2, p, a");
+
+//h2
+let titre = document.createElement("h2");
+let titreH2 = document.createTextNode("Who uses HTML5 Boilerplate?");
+titre.appendChild(titreH2);
+document.body.appendChild(titre);
+
+//p
+let in-the-wildP = document.createElement("P");
+let paragraphe = document.createTextNode("...");
+in-the-wildP.appendChild(paragraphe);
+document.body.appendChild(in-the-wildP);
+
+//a href microsoft
+let a = document.createElement("A");
+let nomLink = document.createTextNode("Microsoft");
+a.setAttribute("href", "https://www.microsoft.com/surface/");
+a.appendChild(nomLink);
+document.body.appendChild(a);
+
+//a href NASA
+let a = document.createElement("A");
+let nomLink = document.createTextNode("NASA");
+a.setAttribute("href", "https://data.nasa.gov/");
+a.appendChild(nomLink);
+document.body.appendChild(a);
+
+//a href Nike
+let a = document.createElement("A");
+let nomLink = document.createTextNode("Nike");
+a.setAttribute("href", "http://www.nikeskateboarding.com/");
+a.appendChild(nomLink);
+document.body.appendChild(a);
+
+//a href Barack Obama
+let a = document.createElement("A");
+let nomLink = document.createTextNode("Barack Obama");
+a.setAttribute("href", "https://www.barackobama.com/");
+a.appendChild(nomLink);
+document.body.appendChild(a);
+
+//ITV News
+let a = document.createElement("A");
+let nomLink = document.createTextNode("ITV News");
+a.setAttribute("href", "https://www.itv.com/news/");
+a.appendChild(nomLink);
+document.body.appendChild(a);
+
+//Creative Commons
+let a = document.createElement("A");
+let nomLink = document.createTextNode("Creative Commons"
+a.setAttribute("href", "https://creativecommons.org/");
+a.appendChild(nomLink);
+document.body.appendChild(a);
+
+//Australia Post
+let a = document.createElement("A");
+let nomLink = document.createTextNode("Australia Post");
+a.setAttribute("href", "https://creativecommons.org/");
+a.appendChild(nomLink);
+document.body.appendChild(a);
+
+//many more
+let a = document.createElement("A");
+let nomLink = document.createTextNode("many more");
+a.setAttribute("href", "https://github.com/h5bp/html5-boilerplate/wiki/sites");
+a.appendChild(nomLink);
+document.body.appendChild(a);
+
+//cta-option
+let cta-optionDiv = document.createElement('div');
+cta-optionDiv.setAttributes('a btn');
+
+//a href
+let btn = document.createElement("A");
+a.setAttribute("href", "https://github.com/h5bp/html5-boilerplate/releases/download/v8.0.0/html5-boilerplate_v8.0.0.zip");
+document.creatElement('strong');
+
+//span
+let versionSpan = document.createElement('span');
+let nomLink = document.createTextNode("v8.0.0");
+
+//aside
+let asideDiv = document.createElement('div');
+asideDiv.className = 'container unline-block-list';
+
+//container
+let containerDiv = document.createElement('div');
+container.setAttributes('ul');
+
+//footer
+let site-footerFooter = document.createElement('footer');
+site-footerFooter.setAttributes('id src');
+let site-footerP = document.createElement("P");
+let paragraphe = document.createTextNode("...");
+let paragraphe = document.createTextNode("...");
+let paragraphe = document.createTextNode("...");
+site-footerP.appendChild(paragraphe);
+document.body.appendChild(site-footerP);
+
+//javascript
+var javascript = document.createElement("SCRIPT");
+var fonction = document.createTextNode("alert('Hello World!')");
+javascript.appendChild(fonction);
+document.body.appendChild(javascript);
+
+var iframeScrolling = document.createElement("IFRAME");
+iframeScrolling.setAttribute("src",  "https://platform.twitter.com/widgets/widget_iframe.96fd96193cc66c3e11d4c5e4c7c7ec97.html?origin=https%3A%2F%2Fhtml5boilerplate.com");
+document.body.appendChild(x);
+}
